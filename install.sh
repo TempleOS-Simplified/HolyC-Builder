b3c45cddc94b380d7e1cf2b281093ac280daa4a7
xzntrc <xzntrc@fedora.email>
2022-10-26 21:31:30 +1100

    Close to final

--- install.sh
+++ install.sh
@@ -1,6 +1,25 @@
 BININSTALL="/usr/local/bin/"
-LIBINSTALL="/usr/local/lib"
+LIBINSTALL="/usr/local/lib/redseagen"
 
-printf "\033[33;1m HolyC Builder\033[0m\n"
+printf "\033[33;1mHolyC Builder\033[0m\n"
+
+read -p "Would you like to install? (Y/N): " -n 1 -r
+echo ""
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+
 echo "HolyC Builder will now install the required binaries..."
-echo "---CURL---"
+echo -ne "Installing Binaries...\r"
+# WRAPPER
+sudo chmod a+x holyc
+sudo cp holyc $BININSTALL
+
+# RedSeaGen Installer
+sudo chmod a+x redseagen
+sudo cp redseagen $LIBINSTALL
+
+# TempleOS // For Virt
+sudo cp templeos.iso /usr/local/lib/templeos.iso
+echo "Finished               "
+fi
+
