83c5871e7423575aeb33138213982e11d36d9962
xzntrc <xzntrc@fedora.email>
2022-10-26 19:15:28 +1100

    Basic file placeholder

---
+++ install.sh
@@ -0,0 +1,6 @@
+BININSTALL="/usr/local/bin/"
+LIBINSTALL="/usr/local/lib"
+
+printf "\033[33;1m HolyC Builder\033[0m\n"
+echo "HolyC Builder will now install the required binaries..."
+echo "---CURL---"
